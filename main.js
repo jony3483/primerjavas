66856e1a2ab00417387340baab6b0b979e6592e9
jony <jony.rpm@gmail.com>
2023-08-01 12:36:39 -0400

    correcion while

--- main.js
+++ main.js
@@ -1,91 +1,31 @@
 
-/*let numero = prompt("elije tu hora nº1 odontologia, nº2 medicina general, nº3 traumatologia");
-
-
-while(numero != "ESC"){
-    switch (numero){
-        case "1":
-            alert("odontologia");
-            break;
-        case "2":
-            alert("medicina general");
-            break;
-        case "3":
-            alert("traumatologia");
-            break;            
-        default:
-            alert("no hay especialidad");
-            return
-    
-    }
- //  let seguro = prompt("elije seguro nº1 fonosa , nº2 isapre");
-}
-    while(seguro != "ESC"){
-        switch (seguro){
-            case "1":
-                alert("fonsas");
-                break;
-            case "2":
-                alert("isapre");
-                break;           
-            default:
-                alert("no hay seguro");
-                break;
-        }
-    }*/  
-
-  /*  function consulta(numero,opcion){
-        switch (numero) {
-            case "1":
-                alert("odontologia");
-                break;
-            case 2:
-                alert("medicina");
-                break;
-            case 3:
-                alert("traumatologia");
-                break;
-            default:
-                alert("elija su especialidad");
-                break;
-        }
-        let numero = parseInt (prompt("elija su consulta nº1 odontologia nº medicina nº traumatologia")); 
-    }*/
-
-
-
-
     function datosPasiente(nombre, edad, direccion,num) {
         alert(`${nombre} ${edad} ${direccion} ${num}`);
     }
-    alert("ingrese especi 1odontologia 2medicina 3traumatologia");
-    let num = prompt("ingrese opcion medica");
+    alert("ingrese especialidad : 1) odontologia, 2) medicina general, 3) traumatologia");
+    let num = parseIntprompt("ingrese opcion medica");
         
         
-       
-    let salir = prompt("ingrese nº 7 para salir");
-        while (salir == 7);{
+    let salir = parseInt(prompt("ingrese el numero 7 para salir"));
+        while (salir === 7){
             if(num == 1){
                 alert("odontologia");
-                }else if (num == 2){
-                alert("medi");
-                }else if (num == 3){
-                alert("trau");
-                }else{
-                alert("selecione medico");
-                num = prompt("ingrese opcion");
-                }
-           
+            }else if (num == 2){
+                alert("medicina general");
+            }else if (num == 3){
+                alert("traumatologia");
+            }else{
+                alert("seleccione especialidad valida");
+                num = prompt("ingrese opcion medica");
+            }
+            salir = parseInt(prompt(" ingrese el numero 7 para salir"));
         }
         
-    
-
-
         alert("ingrese datos del paciente");
         let nombre = prompt("ingrese nombre");
         let edad = prompt("ingrese  edad");
         let direccion = prompt("ingrese su direccion");
-    datosPasiente(nombre, edad, direccion, num);
+        datosPasiente(nombre, edad, direccion, num);
 
 
 
